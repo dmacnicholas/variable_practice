dedbecc0e5d7984caa0337b0c744afb51ccae091
Drew MacNicholas <dmacnicholas@gmail.com>
2022-04-13 21:21:55 -0500

    Update String Values

--- variables.rb
+++ variables.rb
@@ -4,14 +4,21 @@ middle_name = 'Henry'
 city = 'Nashville'
 state = 'Tennessee'
 
+first_name = 'Sally'
+middle_name = 'E'
+
+
+# variable output is assigned to the integer of "spouse"
 Spouse = 1
 Children = 3
 Cars = 2
 
+# variable output is assigned to the float of "car payment"
 Car_Payment = 250.00
 Mortgage = 3000.00
 Utilies = 500.00
 Phone = 200.00
 
+# variable output is assigned to the boolean of "Mowed_Lawn"
 Mowed_Lawn = True
 Washed_Car = False
